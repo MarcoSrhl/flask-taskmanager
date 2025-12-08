b9a8b49f091a3bbced5a2adc44d9feb22f0e9c59
Marco-Naji Serhal <marcoserhal@icloud.com>
2025-12-08 01:24:20 +0100

    test

--- app.py
+++ app.py
@@ -226,4 +226,4 @@ def register_routes(app):
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=True,use_reloader=False, port=5000, host="0.0.0.0")
